d5dbc623c8360ba777a110e89be7f28e0565ee19
Kenji Funaoka <kfunaoka@gmail.com>
2018-10-02 17:48:35 +0900

    Set html_theme sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -180,3 +180,4 @@ epub_exclude_files = ['search.html']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
